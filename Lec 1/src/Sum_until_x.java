741257b364eb78130ac445f953dec2c75b37ff43
TarunRaveesh <tarunraveesh07@gmail.com>
2023-01-23 16:31:34 +0530

    Commit

--- Lec 1/src/Sum_until_x.java
+++ Lec 1/src/Sum_until_x.java
@@ -1,5 +1,3 @@
-package src;
-
 import java.util.Scanner;
 
 public class Sum_until_x {
---
+++ Live Practice/src/Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter Operator: ");
+            char ch = in.next().trim().charAt(0);
+            if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%') {
+                System.out.print("Enter two numbers: ");
+                int n1 = in.nextInt();
+                int n2 = in.nextInt();
+                if (ch == '+') {
+                    System.out.println("Add = " + (n1 + n2));
+                } else if (ch == '-') {
+                    System.out.println("Sub = " + (n1 - n2));
+                } else if (ch == '*') {
+                    System.out.println("Mul = " + (n1 * n2));
+                } else if (ch == '/') {
+                    System.out.println("Div = " + (n1 / n2));
+                } else {
+                    System.out.println("Mod = " + (n1 % n2));
+                }
+            } else if (ch == 'x' || ch == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid operation. Try again.");
+                
+            }
+        }
+    }
+}
