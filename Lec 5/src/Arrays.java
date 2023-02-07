85da28879ed9460af4c1ca60ff644de74e5f5b4c
TarunRaveesh <tarunraveesh07@gmail.com>
2023-02-07 21:34:27 +0530

    Commit

---
+++ Lec 5/src/Arrays.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int [5];
+        for (i) {
+            num = in.nextInt();
+        }
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
