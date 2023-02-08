730379dbb1286bc10198c81ba8c5eae64de9466a
TarunRaveesh <tarunraveesh07@gmail.com>
2023-02-08 09:52:17 +0530

    Commit

--- Lec 5/src/Array.java
+++ Lec 5/src/Array.java
@@ -1,14 +1,16 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Arrays {
+public class Array {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[] arr = new int [5];
-        for (i) {
-            num = in.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
         }
         for(int num : arr) {
             System.out.print(num + " ");
         }
+        System.out.println("\n" + Arrays.toString(arr));
     }
 }
