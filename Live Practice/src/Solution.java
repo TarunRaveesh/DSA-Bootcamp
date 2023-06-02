24c65df189133c17ea550d0772723caa0c7cc076
Tarun Raveesh <tarunraveesh07@gmail.com>
2023-06-03 02:14:36 +0530

    Commit

---
+++ Live Practice/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int Sum = 0, digits = 0;
+        for (int i = 0; i < n; i++) {
+            int s = in.nextInt();
+            int[] arr = new int[s];
+            for (int j = 0; j < s; j++) {
+                arr[j] = in.nextInt();
+                Sum += arr[j];
+            }
+            Sum += 1;
+            while(Sum > 0) {
+                
+            }
+            for (int j = 0; j < ; j++) {
+
+            }
+        }
+    }
+}
