b5a554cdb8f85fa0e9397081133722f9e5fb82d5
TarunRaveesh <tarunraveesh07@gmail.com>
2024-03-09 13:27:09 +0530

    Commit

---
+++ 7. Sorting/src/CyclicSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 4, 1, 2};
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cyclic(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
