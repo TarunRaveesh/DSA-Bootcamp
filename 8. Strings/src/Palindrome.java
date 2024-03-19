f73f1b527cfd6c765dbe38d81bd9b4f49c9d27c4
TarunRaveesh <tarunraveesh07@gmail.com>
2024-03-19 17:38:37 +0530

    Commit

---
+++ 8. Strings/src/Palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "abccbba";
+        System.out.println(isPalindrome1(str));
+        System.out.println(isPalindrome2(str));
+    }
+
+    static boolean isPalindrome1(String str) {
+        // Method 1 - String Builder
+        str = str.toLowerCase();
+        String rev = new StringBuilder(str).reverse().toString();
+        return str.equals(rev);
+    }
+
+    static boolean isPalindrome2(String str) {
+        // Method 2 - Two Pointer
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char s = str.charAt(i);
+            char e = str.charAt(str.length() - 1 - i);
+            if (s != e) return false;
+        }
+        return true;
+    }
+}
