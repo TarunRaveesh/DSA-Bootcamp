6ab9ddb9102cbcf719c1f4984af4107642dd5af3
TarunRaveesh <tarunraveesh07@gmail.com>
2024-03-20 15:21:45 +0530

    Commit

--- 10. Recursion/src/BinarySearch.java
+++ 10. Recursion/src/BinarySearch.java
@@ -1,2 +1,19 @@
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 7, 16, 21, 45, 52};
+        System.out.println(search(arr, 52, 0, arr.length -1));
+    }
+    static int search(int[] arr, int target, int s, int e) {
+        if (s > e) {
+          return -1;
+        }
+        int m = s + (e - s) / 2;
+        if (arr[m] == target) {
+            return m;
+        } if (arr[m] < target) {
+            return search(arr, target, m + 1, e);
+        } else {
+            return search(arr, target, s, m - 1);
+        }
+    }
 }
