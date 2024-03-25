47b4073af1843853b59b10a780a759fa70982382
TarunRaveesh <tarunraveesh07@gmail.com>
2024-03-25 22:28:56 +0530

    Commit

--- 10. Recursion/src/PrintNum.java
+++ 10. Recursion/src/PrintNum.java
@@ -1,17 +1,29 @@
 public class PrintNum {
     public static void main(String[] args) {
-        print(1);
+        print(10);
+        System.out.println();
+        revPrint(10);
     }
 
     static void print(int n) {
         // Base Condition
-        if (n == 10) {
-            System.out.print(10 + " ");
+        if (n == 0) {
             return;
         }
 
         //Recursive Condition
         System.out.print(n + " ");
-        print(n + 1); // Since this function is being called again and again, each time it will occupy separate space in the call stack.
+        print(n - 1); // Since this function is being called again and again, each time it will
+                        // occupy separate space in the call stack.
+    }
+
+    static void revPrint(int n) {
+        // Base Condition
+        if (n == 0) {
+            return;
+        }
+        //Recursive Condition
+        revPrint(n - 1);
+        System.out.print(n + " ");
     }
 }
