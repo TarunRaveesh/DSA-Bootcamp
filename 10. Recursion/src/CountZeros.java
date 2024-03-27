d3dab1a8928e4c57d205dae989b8348c4191f9a9
TarunRaveesh <tarunraveesh07@gmail.com>
2024-03-27 16:57:19 +0530

    Commit

---
+++ 10. Recursion/src/CountZeros.java
@@ -0,0 +1,17 @@
+public class CountZeros {
+    public static void main(String[] args) {
+        int n = 2003000903;
+        System.out.println(count(n));
+    }
+    static int count(int n) {
+        return helper(n, 0);
+    }
+
+    // Special Example, how to pass a value to above calls.
+    static int helper(int n, int c) {
+        if(n == 0) return c;
+        int rem = n % 10;
+        if(rem == 0) return helper(n/10, ++c);
+        else return helper(n/10, c);
+    }
+}
