b79bc6691950d3ee6ca9b4408e7f2e6361917969
TarunRaveesh <tarunraveesh07@gmail.com>
2024-03-28 08:46:24 +0530

    Commit

--- 10. Recursion/Arrays/src/CheckSortedArray.java
+++ 10. Recursion/Arrays/src/CheckSortedArray.java
@@ -1,2 +1,10 @@
 public class CheckSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 7, 10, 23};
+        System.out.println(sorted(arr, 0));
+    }
+    static boolean sorted(int[] arr, int p) {
+        if (p == arr.length - 1) return true;
+        return (arr[p] < arr[p + 1]) && (sorted(arr, ++p));
+    }
 }
