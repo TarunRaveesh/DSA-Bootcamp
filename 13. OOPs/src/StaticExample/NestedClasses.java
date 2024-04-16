3c2cd671192ad136bd8bd5cf982da7b3b6f38202
TarunRaveesh <tarunraveesh07@gmail.com>
2024-04-16 23:18:00 +0530

    Commit

---
+++ 13. OOPs/src/StaticExample/NestedClasses.java
@@ -0,0 +1,21 @@
+package StaticExample;
+
+public class NestedClasses {
+    // This child class has to be static else it will be dependent on the objects of the parent class.
+    static class Test {
+        String name;
+        public Test(String name) {
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        // We cannot instantiate the object if the above Test Class is not static.
+        // This is because if non-static, the Test class will be dependent on the objects of the parent class.
+        Test obj1 = new Test("Tarun");
+        Test obj2 = new Test("Pawan");
+
+        System.out.println(obj1.name);
+        System.out.println(obj2.name);
+    }
+}
--- 13. OOPs/src/StaticExample/StaticMain.java
+++ 13. OOPs/src/StaticExample/StaticMain.java
@@ -1,6 +1,6 @@
 package StaticExample;
 
-public class Main {
+public class StaticMain {
     public static void main(String[] args) { // The 'main' method is static bcz it is the first thing that Java runs,
         // so we must be able to run it without creating an object of the class that has the 'main method'. Hence, basically
         // a static variable or a method is a property of the class instead of the object.
@@ -18,7 +18,7 @@ public class Main {
         //But fun2 is also non-static, and its object will be definitely created in the static main method
         // which in turn which will also be used by greeting() like below:
         System.out.print("Non-Static Method inside Non-Static Method: ");
-        Main obj2 = new Main();
+        StaticMain obj2 = new StaticMain();
         obj2.fun2();
     }
 
@@ -27,7 +27,7 @@ public class Main {
         // greeting(); // Can't use it inside this static function as this method is non-static and requires an instance
 
         // Cannot access non-static elements without referencing their instances in a static context
-        Main obj = new Main();
+        StaticMain obj = new StaticMain();
         obj.greeting();
     }
 
@@ -37,7 +37,7 @@ public class Main {
 
     // We know that something which is not static belongs to an object
     void greeting() {
-        //fun(); // This is possible static inside non-static, not a big deal
+        //fun(); // This is possible, static inside non-static, not a big deal
         System.out.println("Hello World");
     }
 }
