8f0a069fd039c652e464d4c1043eab15fe101fd9
TarunRaveesh <tarunraveesh07@gmail.com>
2024-04-23 21:27:44 +0530

    Commit

--- 13. OOPs/src/Properties/Polymorphism/Circle.java
+++ 13. OOPs/src/Properties/Polymorphism/Circle.java
@@ -1,10 +1,10 @@
 package Properties.Polymorphism;
 
-public class Circle extends Shapes{
-
+public class Circle extends Shapes {
     // This will run when object of circle is created
     // Hence, it is overriding the parent method
-    @Override // Annotation, to check if the given method is actually being overridden
+    @Override
+    // Annotation, to check if the given method is actually being overridden
     void area() {
         System.out.println("pi * r * r");
     }
--- 13. OOPs/src/Properties/Polymorphism/Triangle.java
+++ 13. OOPs/src/Properties/Polymorphism/Triangle.java
@@ -1,5 +1,4 @@
 package Properties.Polymorphism;
-
 public class Triangle extends Shapes{
     void area() {
         System.out.println("1/2 * H * B");
