43078c2503a92def6884aac2c883e343870c24a8
TarunRaveesh <tarunraveesh07@gmail.com>
2024-04-24 11:24:57 +0530

    Commit

--- 13. OOPs/src/Properties/Encapsulation/ObjectClass/ObjectDemo.java
+++ 13. OOPs/src/Properties/Encapsulation/ObjectClass/ObjectDemo.java
@@ -1,9 +1,14 @@
 package Properties.Encapsulation.ObjectClass;
 
-public class ObjectDemo {
+import Properties.Encapsulation.AccessSpecifiers.Access;
 
-    public ObjectDemo() {
-        super();
+public class ObjectDemo {
+    int x;
+    double y;
+
+    public ObjectDemo(int x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
     @Override
@@ -13,7 +18,7 @@ public class ObjectDemo {
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        return this.x == ((ObjectDemo)obj).x; // Object Type Casting
     }
 
     @Override
@@ -33,11 +38,17 @@ public class ObjectDemo {
     }
 
     public static void main(String[] args) throws Throwable {
-        ObjectDemo obj = new ObjectDemo();
+        ObjectDemo obj = new ObjectDemo(4, 2.8);
+        ObjectDemo obj2 = new ObjectDemo(4, 7.2);
         System.out.println(obj.toString());
         System.out.println(obj.hashCode());
-        System.out.println(obj.equals(obj));
-        System.out.println(obj.clone());
+        System.out.println(obj.equals(obj2));
+//        System.out.println(obj.clone());
 //        System.out.println(obj.finalize());
+
+        System.out.println(obj instanceof ObjectDemo);
+        System.out.println(obj instanceof Object);
+        System.out.println(obj.getClass().getName()); // Can't override getClass method(Cz final)
+        // getClass() info stored in heap
     }
 }
