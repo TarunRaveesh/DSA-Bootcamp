b1d74aa7328a1e75fec52d54b950bb8d82d1ad56
TarunRaveesh <tarunraveesh07@gmail.com>
2024-04-25 22:21:38 +0530

    Commit

--- 13. OOPs/src/ExceptionHandling/Demo.java
+++ 13. OOPs/src/ExceptionHandling/Demo.java
@@ -2,15 +2,7 @@ package ExceptionHandling;
 
 public class Demo {
     public static void main(String[] args) {
-        divide(24, 2);
-        divide(4, 0);
-
-    }
-
-    static void divide(int a, int b) throws ArithmeticException {
-        if(b == 0) {
-            throw new ArithmeticException("Division by Zero");
-        }
-        System.out.println(a/b);
+        Main.divide(24, 2);
+        Main.divide(4, 0);
     }
 }
--- 13. OOPs/src/ExceptionHandling/Main.java
+++ 13. OOPs/src/ExceptionHandling/Main.java
@@ -2,14 +2,24 @@ package ExceptionHandling;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 0;
         try {
-            System.out.println(a/b);
-        } catch (Exception e) {
+            divide(15, 5);
+            divide(6,0);
+            // Mimicking an exception
+            // throw new Exception("Just for fun"); // Custom Exception
+        } catch (ArithmeticException e) { // Stricter Rules must come above
             System.out.println(e.getMessage());
-        } finally {
+        } catch (Exception e) { // We can use multiple catch blocks
+            System.out.println("Normal Exception");
+        }  finally {
             System.out.println("This will always Execute.");
         }
+    }
+
+    static void divide(int a, int b) throws ArithmeticException {
+        if(b == 0) {
+            throw new ArithmeticException("Division by Zero");
+        }
+        System.out.println(a/b);
     }
 }
