d716c54f2c91e861681270631a2fd1fcbcd6addd
TarunRaveesh <tarunraveesh07@gmail.com>
2024-04-29 10:22:57 +0530

    Commit

--- 12. DSA Maths/src/PascalTriangleRowSum.java
+++ 12. DSA Maths/src/PascalTriangleRowSum.java
@@ -4,6 +4,6 @@ public class PascalTriangleRowSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
-        System.out.println(1 << (row-1));
+        System.out.println(1 << (row - 1));
     }
 }
