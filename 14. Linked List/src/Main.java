907cb464e26314f9ec74307142ae8f3da6550b6d
TarunRaveesh <tarunraveesh07@gmail.com>
2024-05-16 21:43:07 +0530

    Commit

--- 14. Linked List/src/Main.java
+++ 14. Linked List/src/Main.java
@@ -1,33 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-//        LL list = new LL();
-//        list.addStart(3);
-//        list.addStart(5);
-//        list.addStart(7);
-//        list.addStart(9);
-//        list.addEnd(12);
-//        list.addStart(1);
-//        list.add(4, 3);
-//        list.add(17, 0);
-//
-//        list.display();
-//        System.out.println(list.length());
-//
-//        System.out.println(list.removeStart());
-//        list.display();
-//
-//        System.out.println(list.removeEnd());
-//        list.display();
-//
-//        System.out.println(list.remove(2));
-//        list.display();
-//
-//        System.out.println(list.remove(0));
-//        list.display();
-//
-//        System.out.println(list.remove(list.length() - 1));
-//        list.display();
+        System.out.println("Linked List");
+        LL list = new LL();
+        list.addStart(3);
+        list.addStart(5);
+        list.addStart(7);
+        list.addStart(9);
+        list.addEnd(12);
+        list.addStart(1);
+        list.add(4, 3);
+        list.add(17, 0);
 
+        list.display();
+        System.out.println(list.length());
+
+        System.out.println(list.removeStart());
+        list.display();
+
+        System.out.println(list.removeEnd());
+        list.display();
+
+        System.out.println(list.remove(2));
+        list.display();
+
+        System.out.println(list.remove(0));
+        list.display();
+
+        System.out.println(list.remove(list.length() - 1));
+        list.display();
+
+        System.out.println("Doubly Linked List");
         DLL dll = new DLL();
         dll.addStart(3);
         dll.addStart(5);
