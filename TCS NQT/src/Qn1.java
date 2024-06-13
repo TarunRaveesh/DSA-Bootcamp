c6ab896e099f559dae4e694d21e93147fd8f896c
TarunRaveesh <tarunraveesh07@gmail.com>
2024-06-13 22:10:16 +0530

    Commit

---
+++ TCS NQT/src/Qn1.java
@@ -0,0 +1,20 @@
+// Qns Link - https://prepinsta.com/tcs-nqt/placement-papers/advance/coding/
+
+import java.util.*;
+public class Qn1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int f = sc.nextInt();
+        int[] arr = new int[f];
+        for(int i = 0; i < f; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        for(int guess : arr) {
+            if(guess != arr[0]) {
+                count += 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
