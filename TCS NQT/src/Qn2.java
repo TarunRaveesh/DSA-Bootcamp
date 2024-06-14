fb611da8bf1dcebd82b9c4b764fb6a3ba6fb6d90
TarunRaveesh <tarunraveesh07@gmail.com>
2024-06-14 11:18:36 +0530

    Commit

--- TCS NQT/src/Qn2.java
+++ TCS NQT/src/Qn2.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Qn2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] prices = new int[n];
+       int maxsum = 0;
 
+        for (int i = 0; i < n; i++) {
+            int currsum = 0;
+            int j = 0;
+            while(maxsum <= 100) {
+                currsum += prices[j];
+                maxsum = Math.max(maxsum, currsum);
+                j++;
+            }
+        }
+
+        System.out.println(maxsum);
     }
 }
---
+++ TCS NQT/src/SlidingWindow/BruteForce.java
@@ -0,0 +1,17 @@
+package SlidingWindow;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        int k = 2;
+        int[] arr = {3, 5, 8, 7, 5 ,12 , 4};
+        int maxsum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length - k; i++) {
+            int currsum = 0;
+            for (int j = 0; j < k; j++) {
+                currsum += arr[i + j];
+                maxsum = Math.max(currsum, maxsum);
+            }
+        }
+        System.out.println(maxsum);
+    }
+}
