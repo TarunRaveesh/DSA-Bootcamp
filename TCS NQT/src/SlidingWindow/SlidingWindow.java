c2fc3572cb29f487f475b7ca18c4e42dd56d6e6f
TarunRaveesh <tarunraveesh07@gmail.com>
2024-06-14 15:56:48 +0530

    Commit

---
+++ TCS NQT/src/SlidingWindow/SlidingWindow.java
@@ -0,0 +1,21 @@
+package SlidingWindow;
+
+public class SlidingWindow {
+    public static void main(String[] args) {
+        int k = 2;
+        int[] arr = {3, 5, 8, 7, 5, 12, 4};
+
+        int currsum = 0;
+
+        for (int i = 0; i < k; i++) {
+            currsum += arr[i];
+        }
+        int maxsum = currsum;
+        for (int i = k; i < arr.length; i++) { // Sliding Window
+            currsum -= arr[i - k];
+            currsum += arr[i];
+            maxsum = Math.max(maxsum, currsum);
+        }
+        System.out.println(maxsum);
+    }
+}
