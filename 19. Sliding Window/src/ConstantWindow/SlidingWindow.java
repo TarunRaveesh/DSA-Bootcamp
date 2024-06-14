ed4cd2e416bdd82fa112fae34da916acc9e61dbe
TarunRaveesh <tarunraveesh07@gmail.com>
2024-06-14 20:58:39 +0530

    Commit

--- 19. Sliding Window/src/ConstantWindow/SlidingWindow.java
+++ 19. Sliding Window/src/ConstantWindow/SlidingWindow.java
@@ -4,18 +4,24 @@ public class SlidingWindow {
     public static void main(String[] args) {
         int k = 2;
         int[] arr = {3, 5, 8, 7, 5, 12, 4};
+        System.out.println(constantSlidingWindow(arr, k));
+    }
 
-        int currsum = 0;
-
+    static int constantSlidingWindow(int[] arr, int k) {
+        int n = arr.length;
+        int l = 0, r = k - 1;
+        int sum = 0;
         for (int i = 0; i < k; i++) {
-            currsum += arr[i];
+            sum += arr[i];
         }
-        int maxsum = currsum;
-        for (int i = k; i < arr.length; i++) { // Sliding Window
-            currsum -= arr[i - k];
-            currsum += arr[i];
-            maxsum = Math.max(maxsum, currsum);
+        int maxsum = sum;
+        while(r < (n - 1)) {
+            sum -= arr[l];
+            l++;
+            r++;
+            sum += arr[r];
+            maxsum = Math.max(maxsum, sum);
         }
-        System.out.println(maxsum);
+        return maxsum;
     }
 }
