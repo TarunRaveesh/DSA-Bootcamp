890dce041b61eefd9c5978fe58cc7148923357ce
TarunRaveesh <tarunraveesh07@gmail.com>
2024-06-15 18:12:02 +0530

    Commit

--- TCS NQT/src/Qn4.java
+++ TCS NQT/src/Qn4.java
@@ -1,15 +1,46 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Qn4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] arr = new int[n][2];
+        HashSet<String> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
             arr[i][0] = sc.nextInt();
             arr[i][1] = sc.nextInt();
+            set.add(arr[i][0] + ":" + arr[i][1]);
         }
-        System.out.println(Arrays.deepToString(arr));
+
+        System.out.println(findSquareCoordinates(arr, set));
+    }
+
+    public static int findSquareCoordinates(int[][] arr, HashSet<String> set) {
+        int n = arr.length;
+        int reqPoints = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int x1 = arr[i][0];
+                int y1 = arr[i][1];
+                int x2 = arr[j][0];
+                int y2 = arr[j][1];
+
+                int dx = Math.abs(x2 - x1);
+                int dy = Math.abs(y2 - y1);
+
+                // Possible Square1 (x1, y1) (x2, y2) (x1 - dy, y1 + dx) (x2 - dy, y2 + dx)
+                int add1 = 0;
+                if(!set.contains((x1 - dy) + ":" + (y1 + dx))) add1++;
+                if(!set.contains((x2 - dy) + ":" + (y2 + dx))) add1++;
+                reqPoints = Math.min(reqPoints, add1);
+
+                // Possible Square2 (x1, y1) (x2, y2) (x1 + dy, y1 - dx) (x2 + dy, y2 - dx)
+                int add2 = 0;
+                if(!set.contains((x1 + dy) + ":" + (y1 - dx))) add2++;
+                if(!set.contains((x2 + dy) + ":" + (y2 - dx))) add2++;
+                reqPoints = Math.min(reqPoints, add2);
+            }
+        }
+        return reqPoints == Integer.MAX_VALUE ? 0 : reqPoints;
     }
 }
---
+++ TCS NQT/src/Qn5.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Qn5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] board = new int[n];
+        int[] students = new int[n];
+        for (int i = 0; i < n; i++) {
+            board[i] = sc.nextInt();
+            students[i] = i + 1;
+        }
+        System.out.println(countDrumBeats(board, students));
+    }
+
+    static int countDrumBeats(int[] B, int[] stud) {
+        int beats = 0;
+        int n = B.length;
+        do {
+            beats++;
+            int[] temp = new int[n];
+            for (int i = 0; i < n; i++) {
+                temp[i] = stud[B[i] - 1];
+            }
+            stud = temp;
+        } while (!isSorted(stud));
+        return beats;
+    }
+
+    private static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if(!(arr[i] > arr[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
