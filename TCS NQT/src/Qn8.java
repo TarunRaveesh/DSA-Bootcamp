767b0e78bd1c20e3f75edd86f22a2983b1e2c47b
TarunRaveesh <tarunraveesh07@gmail.com>
2024-06-16 02:51:25 +0530

    Commit

--- TCS NQT/src/Qn8.java
+++ TCS NQT/src/Qn8.java
@@ -1,2 +1,14 @@
+import java.util.*;
 public class Qn8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int res = 1;
+        while(n > 0) {
+            res = res*(n%10);
+            n /= 10;
+        }
+        System.out.println(res);
+        System.out.println('1' - '0');
+    }
 }
