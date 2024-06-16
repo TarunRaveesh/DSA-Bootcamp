511ee95c17e7920a857758445094375239ba3889
TarunRaveesh <tarunraveesh07@gmail.com>
2024-06-16 14:01:24 +0530

    Commit

--- 12. DSA Maths/src/SieveOfEratosthenes.java
+++ 12. DSA Maths/src/SieveOfEratosthenes.java
@@ -10,9 +10,7 @@ public class SieveOfEratosthenes { // Time: O(log(log(n)) Space:O(n)
         boolean[] primes = new boolean[n+1];
         for (int i = 2; i * i < n; i++) {
             if(!primes[i]) {
-                for(int j = i * i; j <= n; j += i) { // Multiples of Prime are being ignored by setting their
-                                                     // respective index value to false
-
+                for(int j = i * i; j <= n; j += i) { // Multiples of Prime are being ignored
                     primes[j] = true;
                 }
             }
