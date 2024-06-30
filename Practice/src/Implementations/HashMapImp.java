17b619a612570f3e10a5fcaa942401d06a1f9bfe
TarunRaveesh <tarunraveesh07@gmail.com>
2024-07-01 01:22:18 +0530

    Commit

--- Practice/src/Implementations/HashMapImp.java
+++ Practice/src/Implementations/HashMapImp.java
@@ -2,29 +2,6 @@ package Implementations;
 
 import java.util.*;
 
-@SuppressWarnings("unchecked")
 public class HashMapImp <K, V> {
-    
-    private Entity<K, V>[] table;
-    static final int DEFAULT_SIZE = 10; 
-    private int size = 0;
-
-    
-    public HashMapImp() {
-        table = new Entity[DEFAULT_SIZE];
-    }
-
-    private class Entity <K, V> {
-        K key;
-        V value;
-
-        public Entity(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
-
-
 
 }
