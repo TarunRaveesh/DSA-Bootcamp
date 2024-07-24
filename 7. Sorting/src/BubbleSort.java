c261d8da4214603269e4cd77c39414dd016b4cd8
TarunRaveesh <tarunraveesh07@gmail.com>
2024-07-24 22:13:16 +0530

    Commit

--- 7. Sorting/src/BubbleSort.java
+++ 7. Sorting/src/BubbleSort.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {2, 1, 3, 4, 5};
-        bubble(arr);
+        bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
-    public static void bubble(int[] arr) {
+    public static void bubbleSort(int[] arr) {
         boolean swapped;
         for (int i = 0; i < arr.length; i++) {
             swapped = false;
